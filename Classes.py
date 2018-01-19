0ad2b494bd59a330737707e06d980fc0f7cc665a
Josh Frinak <34491710+JFrinak@users.noreply.github.com>
2018-01-19 14:26:59 -0700

    Add files via upload

--- Classes.py
+++ Classes.py
@@ -1,152 +1,157 @@
 from sage.all import *
 import pdb
 import numpy as np
-#--------------------------------------------------------------------------------------------------------------------------------------------------
-#----------------------------------------------------------------------Classes---------------------------------------------------------------------
-#--------------------------------------------------------------------------------------------------------------------------------------------------   
- 
+
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------Classes---------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------------------------------------
+
 class EGraph:
-    def __init__(self,G,IM,configs):
+    def __init__(self, G, IM, configs):
         self.G = G
-	self.IM = IM
-	self.configs = configs
+        self.IM = IM
+        self.configs = configs
         self.Homology_Basis = Matrix(self.IM).right_kernel().basis()
-        self.CC_loops = []
         self.are_LVs_covered = True
 
-    def generate_CC_loops(self,CV,CE):
+    def setNumberCCEdges(self, CV, CE):
         loop_verts = self.G.loop_vertices()
         self.are_LVs_covered = True
         for v in loop_verts:
-            if(not CV[v]):
+            if (not CV[v]):
                 self.are_LVs_covered = False
                 break
-        flag = True
-        #For each basis vector in the homology basis
-        for bv in self.Homology_Basis:
-            flag = True
-            #For each edge in the basis vector
-            for e in range(self.configs["edges"]):
-                #if the edge is actually a part of the cycle
-                if(bv[e]!= 0):
-                    #verify that the edge is covered 
-                    condition_1 = not CE[e]
-                    #verify that if the edge is a loop then the vertex is covered
-                    condition_2 = (e<self.configs["loops"]) and not self.are_LVs_covered
-                    #verify that if edge not a loop then the starting vertex of the edge is covered
-                    condition_3 = (e>=self.configs["loops"]) and not CV[self.start(e)]
-                    #verify that if edge not a loop then the ending vertex of the edge is covered
-                    condition_4 = (e>=self.configs["loops"]) and not CV[self.end(e)]
-                    if(condition_1 or condition_2 or condition_3 or condition_4):
-                        flag = False
-                        break;
-            if(flag):
-                self.CC_loops.append(bv)
-                
-    def start(self,e):
+        self.numberCCEdges = self.configs["loops"]  # Number of completely covered edges including loops
+        self.ccEdges = [False] * self.configs["edges"]  # array of completely covered edges that are not loops
+        # make sure loops are declared cc edges
+        for e in range(self.configs["loops"]):
+            self.ccEdges[e] = True
+        for e in range(self.configs["loops"], self.configs["edges"]):
+            if (CE[e] and CV[self.start(e)] and CV[self.end(e)]):
+                self.numberCCEdges += 1
+                self.ccEdges[e] = True
+
+    def start(self, e):
         for v in range(self.configs["verts"]):
-            if(self.IM[v][e]==-1):
-                return v
-        return -1
-    def end(self,e):
-        for v in range(self.configs["verts"]):
-            if(self.IM[v][e]==1):
+            if (self.IM[v][e] == -1):
                 return v
         return -1
 
-#--------------------------------------------------------------------------------------------------------------------------------------------------
+    def end(self, e):
+        for v in range(self.configs["verts"]):
+            if (self.IM[v][e] == 1):
+                return v
+        return -1
+
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------
 class CoverGraph:
-	def __init__(self,IM,CE,CV,configs):
-		self.configs = configs
-		self.IM = IM
-		self.CE = CE
-		self.CV = CV
-		self.sage_IM = np.copy(self.IM)
-		#remove zero rows
-		self.sage_IM=self.sage_IM[~np.all(self.sage_IM==0,axis=1)]
-		#remove zero cols
-		self.sage_IM=self.sage_IM[:,~np.all(self.sage_IM==0,axis=0)]
-                self.G = Graph(Matrix(self.sage_IM))   
-		self.get_Homology_basis()
-		self.get_Image_CHB()
-		self.find_basis_Linear_Forms()
-		self.check_FS()
-	def get_Homology_basis(self):
-		self.Homology_Basis = list(Matrix(self.IM).right_kernel().basis())
-		for bv in self.Homology_Basis:
-			count = 0 
-			for e in range(2*self.configs["edges"]):
-				if(bv[e]==0):
-					count+=1
-			#remove uncovered edges
-			if(count==2*self.configs["edges"]-1):
-				self.Homology_Basis.remove(bv)
-	def get_Image_CHB(self):
-		#get identity - involution matrix
-		Identity_Minus_Edge_Involution = np.zeros((2*self.configs["edges"],2*self.configs["edges"]),dtype=np.int)
-		for i in range(self.configs["edges"]):
-			if(self.CE[i]):
-				Identity_Minus_Edge_Involution[2*i+1][2*i]=-1
-				Identity_Minus_Edge_Involution[2*i][2*i+1]=-1
-				Identity_Minus_Edge_Involution[2*i][2*i]=1
-				Identity_Minus_Edge_Involution[2*i+1][2*i+1]=1
-		HB_matrix = np.matrix(self.Homology_Basis)
-		Unreduced_Image = np.dot(HB_matrix,Identity_Minus_Edge_Involution)
-		self.Image_CHB = Matrix(ZZ,Unreduced_Image).echelon_form()
-		zero_rows = []
-		for row in range(self.Image_CHB.nrows()):
-			is_zero = True
-			for col in range(2*self.configs["edges"]):
-				if(self.Image_CHB[row,col]!=0):
-					is_zero = False
-					break
-			if(is_zero):
-				zero_rows.append(row)
-		if(len(zero_rows)>0):
-			self.Image_CHB = self.Image_CHB.delete_rows(zero_rows)
-	def find_basis_Linear_Forms(self):
-		self.Basis_Linear_Forms = []
-		for j in range(self.Image_CHB.nrows()):
-			b_vector = [0]*self.configs["edges"]
-			for i in range(self.configs["edges"]):
-				if(self.CE[i]):
-					b_vector[i]=self.Image_CHB[j][2*i]-self.Image_CHB[j][2*i+1]
-				else:
-					b_vector[i]=self.Image_CHB[j][2*i]
-			self.Basis_Linear_Forms.append(b_vector)
-		#Make vectors primitive, Divide by two if possible
-		for e in range(self.configs["edges"]):
-			while(self.is_divisible_by_two(e)):
-				for z in self.Basis_Linear_Forms:
-					z[e]/=2
-		self.Basis_Linear_Forms = Matrix(self.Basis_Linear_Forms)
-	def is_divisible_by_two(self, e):
-		flag= False
-		for z in self.Basis_Linear_Forms:
-			x = z[e]
-			if(x%2!=0):
-				return False
-			#Need to make sure row is not all zeros to avoid infinite loop
-			if(x!=0):
-				if(not flag):
-					flag=true
-		return flag
+    def __init__(self, IM, CE, CV, configs):
+        self.configs = configs
+        self.IM = IM
+        self.CE = CE
+        self.CV = CV
+        self.sage_IM = np.copy(self.IM)
+        # remove zero rows
+        self.sage_IM = self.sage_IM[~np.all(self.sage_IM == 0, axis=1)]
+        # remove zero cols
+        self.sage_IM = self.sage_IM[:, ~np.all(self.sage_IM == 0, axis=0)]
+        self.G = Graph(Matrix(self.sage_IM))
+        self.get_Homology_basis()
+        self.get_Image_CHB()
+        self.find_basis_Linear_Forms()
+        self.check_FS()
 
-	def check_FS(self):
-		FS = False
-		rank = self.Basis_Linear_Forms.rank()
-		minors = self.Basis_Linear_Forms.minors(rank)
-		units = [0,-1,1] #units and zero of ZZ
-		for m in minors:
-			if(m not in units):
-				FS = true
-		if(FS and rank>3):
-			self.FS = True
-		else:
-			self.FS = False
-	def output(self):
-		string = "Incidence Matrix: \n"
-		string += str(self.IM) + "\n"
-		string += "Basis of Linear Forms: \n"
-		string += str(self.Basis_Linear_Forms) + "\n"
-		return string
+    def get_Homology_basis(self):
+        self.Homology_Basis = list(Matrix(self.IM).right_kernel().basis())
+        for bv in self.Homology_Basis:
+            count = 0
+            for e in range(2 * self.configs["edges"]):
+                if (bv[e] == 0):
+                    count += 1
+            # remove uncovered edges
+            if (count == 2 * self.configs["edges"] - 1):
+                self.Homology_Basis.remove(bv)
+
+    def get_Image_CHB(self):
+        # get identity - involution matrix
+        Identity_Minus_Edge_Involution = np.zeros((2 * self.configs["edges"], 2 * self.configs["edges"]), dtype=np.int)
+        for i in range(self.configs["edges"]):
+            if (self.CE[i]):
+                Identity_Minus_Edge_Involution[2 * i + 1][2 * i] = -1
+                Identity_Minus_Edge_Involution[2 * i][2 * i + 1] = -1
+                Identity_Minus_Edge_Involution[2 * i][2 * i] = 1
+                Identity_Minus_Edge_Involution[2 * i + 1][2 * i + 1] = 1
+        HB_matrix = np.matrix(self.Homology_Basis)
+        Unreduced_Image = np.dot(HB_matrix, Identity_Minus_Edge_Involution)
+        self.Image_CHB = Matrix(ZZ, Unreduced_Image).echelon_form()
+        zero_rows = []
+        for row in range(self.Image_CHB.nrows()):
+            is_zero = True
+            for col in range(2 * self.configs["edges"]):
+                if (self.Image_CHB[row, col] != 0):
+                    is_zero = False
+                    break
+            if (is_zero):
+                zero_rows.append(row)
+        if (len(zero_rows) > 0):
+            self.Image_CHB = self.Image_CHB.delete_rows(zero_rows)
+
+    def find_basis_Linear_Forms(self):
+        self.Basis_Linear_Forms = []
+        for j in range(self.Image_CHB.nrows()):
+            b_vector = [0] * self.configs["edges"]
+            for i in range(self.configs["edges"]):
+                if (self.CE[i]):
+                    b_vector[i] = self.Image_CHB[j][2 * i] - self.Image_CHB[j][2 * i + 1]
+                else:
+                    b_vector[i] = self.Image_CHB[j][2 * i]
+            self.Basis_Linear_Forms.append(b_vector)
+        # Make vectors primitive, Divide by two if possible
+        for e in range(self.configs["edges"]):
+            while (self.is_divisible_by_two(e)):
+                for z in self.Basis_Linear_Forms:
+                    z[e] /= 2
+        self.Basis_Linear_Forms = Matrix(self.Basis_Linear_Forms)
+
+    def is_divisible_by_two(self, e):
+        flag = False
+        for z in self.Basis_Linear_Forms:
+            x = z[e]
+            if (x % 2 != 0):
+                return False
+            # Need to make sure row is not all zeros to avoid infinite loop
+            if (x != 0):
+                if (not flag):
+                    flag = true
+        return flag
+
+    def check_FS(self):
+        FS = False
+        rank = self.Basis_Linear_Forms.rank()
+        minors = self.Basis_Linear_Forms.minors(rank)
+        units = [0, -1, 1]  # units and zero of ZZ
+        for m in minors:
+            if (m not in units):
+                FS = true
+        if (FS and rank > 3):
+            self.FS = True
+        else:
+            self.FS = False
+
+    def output(self):
+        string = "Incidence Matrix: \n"
+        string += str(self.IM) + "\n"
+        string += "Basis of Linear Forms: \n"
+        string += str(self.Basis_Linear_Forms) + "\n"
+        return string
+#---------------------------------------------------------------------------------------------
+class GraphShell:
+    def __init__(self,LIMA,IMA,configs):
+        self.LIMA = LIMA
+        self.IMA = IMA
+        self.configs = configs
+
+
+
---
+++ RecursiveBGGeneration.py
@@ -0,0 +1,248 @@
+# RecursiveBGGeneration.py
+from sage.all import *
+import pdb
+import numpy as np
+from Classes import EGraph, GraphShell
+import sys
+
+configs = {}
+if (sys.argv[0] == None):
+    raise TypeError
+else:
+    configs["edges"] = int(sys.argv[1])
+if (sys.argv[0] == None):
+    raise TypeError
+else:
+    configs["verts"] = int(sys.argv[2])
+if (sys.argv[0] == None):
+    raise TypeError
+else:
+    configs["loops"] = int(sys.argv[3])
+
+
+# ----------------------------------------------------------------------------------------------------------
+def LoadGraphs(verts, edges, loops):
+    # type: (int, int, int) -> [GraphShell]
+    input_file = str(verts) + "V" + str(edges) + "E" + str(loops) + "LBaseGraphs.txt"
+    All_GraphShells = []
+    with open(input_file, "r") as inF:
+        row = 0
+        LIMA = []
+        IMA = []
+        for line in inF:
+            if (line == "\n"):
+                gs = GraphShell(LIMA, IMA, configs)
+                All_GraphShells.append(gs)
+                row = 0
+                LIMA = []
+                IMA = []
+            elif (row == 0):
+                if (loops > 0):
+                    line = line.rstrip()
+                    num = ""
+                    for ch in line:
+                        if (ch not in ['(', ',', ')', '[', ']']):
+                            num += ch
+                        elif (ch in [',', ')', ']']):
+                            if num != "":
+                                integer = int(num)
+                                LIMA.append(integer)
+                                num = ""
+                row += 1
+            elif (row == 1):
+                IMA = []
+                line = line.rstrip()
+                num = ""
+                for ch in line:
+                    if (ch not in ['[', ',', ']']):
+                        num += ch
+                    elif (ch in [',', ']']):
+                        if num != "":
+                            integer = int(num)
+                            IMA.append(integer)
+                            num = ""
+
+    return All_GraphShells
+
+
+def Construct_EGraph_no_loops(APE, IMA):
+    G = DiGraph(configs["verts"], multiedges=true)
+    IM = np.zeros((configs["verts"], configs["edges"]), dtype=np.int)
+    for j in range(configs["edges"]):
+        G.add_edge(APE[IMA[j]][0], APE[IMA[j]][1], j)
+        IM[APE[IMA[j]][0]][j] = -1
+        IM[APE[IMA[j]][1]][j] = 1
+    E = EGraph(G, IM, configs)
+    return E
+
+
+def Construct_EGraph(LIMA, APE, IMA):
+    G = DiGraph(configs["verts"], loops=true, multiedges=true)
+    edge_count = 0
+    IM = np.zeros((configs["verts"], configs["edges"]), dtype=np.int)
+    for k in range(len(LIMA)):
+        for j in range(LIMA[k]):
+            G.add_edge(k, k, edge_count)
+            edge_count += 1
+    for j in range(len(IMA)):
+        G.add_edge(APE[IMA[j]][0], APE[IMA[j]][1], edge_count)
+        IM[APE[IMA[j]][0]][edge_count] = -1
+        IM[APE[IMA[j]][1]][edge_count] = 1
+        edge_count += 1
+    E = EGraph(G, IM, configs)
+    return E
+def swapVertices(APE,IMA,vert1,vert2):
+    #Adjust IMA
+    newIMA = []
+    for e in range(configs["edges"]-configs["loops"]):
+        edge = [x for x in APE[IMA[e]]]
+        newEdge=[0]*2
+        if bool(vert1 in edge) != bool(vert2 in edge):
+            if(edge[0] == vert1 and edge[1]< vert2):
+                newEdge = [edge[1],vert2]
+            elif(edge[0] == vert1 and vert2<edge[1]):
+                newEdge = [vert2,edge[1]]
+            elif(edge[0] == vert2):
+                newEdge = [vert1,edge[1]]
+            elif(edge[1] == vert1):
+                newEdge = [edge[0], vert2]
+            elif(edge[1] == vert2 and edge[0]<vert1):
+                newEdge = [edge[0],vert1]
+            elif(edge[1]==vert2 and vert1<edge[0]):
+                newEdge = [vert1,edge[0]]
+            newEdgeIndex = APE.index(newEdge)
+
+        else:
+            newEdgeIndex = IMA[e]
+
+        if(len(newIMA)==0):
+            newIMA.append(newEdgeIndex)
+        else:
+            for i in range(len(newIMA)+1):
+                if(i == len(newIMA)):
+                    newIMA.append(newEdgeIndex)
+                else:
+                    if(newEdgeIndex<= newIMA[i]):
+                        newIMA.insert(i,newEdgeIndex)
+                        break
+    return newIMA
+
+
+# ----------------------------------------------------------------------------------------------------------
+All_Edges = []
+for i in range(configs["verts"]):
+    for j in range(i + 1, configs["verts"]):
+        A = [0] * 2
+        A[0] = i
+        A[1] = j
+        All_Edges.append(A)
+
+max_value = len(All_Edges) - 1
+IMA = [0] * (configs["edges"]-configs["loops"])
+la = ()
+All_Graphs = []
+All_EGraphs = []
+
+Output_file = str(configs["verts"]) + "V" + str(configs["edges"]) + "E" + str(configs["loops"]) + "LBaseGraphs.txt"
+with open(Output_file, "w") as f:
+    count = 0
+    if configs["edges"]<configs["verts"]:
+        IMA = [x*configs["edges"]-x*(x-1)/2 for x in range(configs["edges"])]
+        E = Construct_EGraph_no_loops(All_Edges, IMA)
+        All_EGraphs.append(E)
+        f.write(str(la) + "\n")
+        f.write(str(IMA) + "\n\n")
+        f.write(str(len(All_EGraphs)) + " Base Graphs")
+
+    elif configs["loops"] == 0:
+        lowerDimensionGraphShells = LoadGraphs(configs["verts"], configs["edges"] - 1, configs["loops"])
+        for shell in lowerDimensionGraphShells:
+            for edge in range(len(All_Edges)):
+                IMA = [x for x in shell.IMA]
+                IMA.append(edge)
+                IMA.sort()
+                E = Construct_EGraph_no_loops(All_Edges, IMA)
+                UG = E.G.to_undirected()
+                if(E.G.degree().count(1)<3):
+                    is_new = True
+                    for g in All_Graphs:
+                        if (UG.is_isomorphic(g)):
+                            is_new = False
+                            break
+                    if (is_new):
+                        All_Graphs.append(UG)
+                        All_EGraphs.append(E)
+                        f.write(str(la) + "\n")
+                        f.write(str(IMA) + "\n\n")
+            count += 1
+            num = float(count) / len(lowerDimensionGraphShells)
+            percent = int(num * 100)
+            string = str(percent) + "%"
+            print(string)
+        f.write(str(len(All_EGraphs)) + " Base Graphs")
+
+    else:
+        lowerDimensionGraphShells = LoadGraphs(configs["verts"], configs["edges"] - 1, configs["loops"] - 1)
+        for shell in lowerDimensionGraphShells:
+            IMA = [x for x in shell.IMA]
+            loopList = [x for x in shell.LIMA]
+
+            for v in range(configs["verts"]):
+                needIMAChange = v>0 and (len(loopList)==0 or (v<len(loopList) and loopList[v]==loopList[v-1])
+                                or v>len(loopList))
+                if needIMAChange:
+                    firstIndex = 0
+                    if len(loopList)==0:
+                        loopList.append(1)
+                        firstindex = 0
+                    elif v<len(loopList) and loopList[v] == loopList[v-1]:
+                        firstIndex = loopList.index(loopList[v]) #firstIndex might not be v-1, i.e. lL = (1,1,1) and v=2
+                        loopList[firstIndex]+=1
+                    elif v>len(loopList):
+                        firstIndex = len(loopList)
+                        loopList.append(1)
+                    else:
+                        raise ValueError('We have not handled all IMA change cases')
+                    newIMA = swapVertices(All_Edges,IMA,firstIndex,v)
+                    E = Construct_EGraph(loopList, All_Edges, newIMA)
+                    UG = E.G.to_undirected()
+                    is_new = True
+                    for g in All_Graphs:
+                        if (UG.is_isomorphic(g)):
+                            is_new = False
+                            break
+                    if (is_new):
+                        All_Graphs.append(UG)
+                        All_EGraphs.append(E)
+                        f.write(str(tuple(loopList)) + "\n")
+                        f.write(str(newIMA) + "\n\n")
+                    loopList = [x for x in shell.LIMA]
+                    IMA = [x for x in shell.IMA]
+
+                else:
+                    if v==0 and len(loopList)==0:
+                        loopList.append(1)
+                    elif v == 0:
+                        loopList[v] += 1
+                    elif v<len(loopList) and loopList[v]<loopList[v-1]:
+                        loopList[v] +=1
+                    elif v == len(loopList):
+                        loopList.append(1)
+                    else:
+                        raise ValueError('We have not handled all non-IMA change cases')
+                    E = Construct_EGraph(loopList, All_Edges, IMA)
+                    UG = E.G.to_undirected()
+                    is_new = True
+                    for g in All_Graphs:
+                        if (UG.is_isomorphic(g)):
+                            is_new = False
+                            break
+                    if (is_new):
+                        All_Graphs.append(UG)
+                        All_EGraphs.append(E)
+                        f.write(str(tuple(loopList)) + "\n")
+                        f.write(str(IMA) + "\n\n")
+                    loopList = [x for x in shell.LIMA]
+                    IMA = [x for x in shell.IMA]
+
+        f.write(str(len(All_EGraphs)) + " Base Graphs")
